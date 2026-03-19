3eb5576962c6cefeae2a8fc0e411eaee05250d7f
osmanbucak1939-dotcom <osmanbucak1939@gmail.com>
2026-03-19 20:21:30 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,2 @@
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
-self.addEventListener('activate', function(e) {
-  self.clients.claim();
-});
+// placeholder
 self.addEventListener('fetch', function(e) {});
