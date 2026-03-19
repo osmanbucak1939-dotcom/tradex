56918a59125411f28e20040e3845b2c32cdb1779
osmanbucak1939-dotcom <osmanbucak1939@gmail.com>
2026-03-19 20:10:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,7 @@
-const CACHE = 'tradex-v1';
-
 self.addEventListener('install', function(e) {
   self.skipWaiting();
 });
-
 self.addEventListener('activate', function(e) {
   self.clients.claim();
 });
-
-self.addEventListener('fetch', function(e) {
-  if (e.request.method !== 'GET') return;
-  if (e.request.url.includes('firebase') || 
-      e.request.url.includes('googleapis')) return;
-  e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request);
-    })
-  );
-});
+self.addEventListener('fetch', function(e) {});
